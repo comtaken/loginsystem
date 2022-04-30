ef0a72d5dc89de264075421f74ef92a53cc67992
comtaken <xxx@xxx.jp>
2022-05-01 00:08:24 +0900

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'env.php';
+function connect()
+{
+//TODO: envファイル用意
+    $host = DB_HOST;
+    $db = DB_NAME;
+    $user = DB_USER;
+    $pass = DB_PASS;
+    try{
+        $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+        
+        $pdo = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+
+       return $pdo;
+    } catch(PDOException $e){
+        echo 'エラー発生: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES) . '<br>';
+        exit;
+    }
+    $dbh = null;
+
+}
+
+?>
